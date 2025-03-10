3e861661b5c41d2b116d85edd8903169ed5ab200
Mcfly <demirurazboyev007@gmail.com>
2025-03-10 20:48:55 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-red-500">Webpack</h1>;
+  return <h1 className="text-5xl text-center mt-[450px] text-red-500">Webpack</h1>;
 };
 
 export default App;
