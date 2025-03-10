864240ecaac00728c0bcb4aaaa3f37251a6f571d
Mcfly <demirurazboyev007@gmail.com>
2025-03-10 20:46:04 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 4000,
+    open: true,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: true,
+    watchFiles: {
+      paths: ["src/**/*.js", "public/index.html"],
+      options: {
+        ignored: /node_modules/,
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
